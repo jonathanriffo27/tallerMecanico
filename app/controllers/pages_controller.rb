606eff7f2393f5763f641084df6172f848d91f54
Jonathan <jonathan.riffo7@gmail.com>
2023-10-13 19:54:51 -0300

    Feat Ususarios con devise y controlador pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about_us
+  end
+
+  def services
+  end
+
+  def schedule
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about_us'
+  get 'pages/services'
+  get 'pages/schedule'
+  get 'pages/contact'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
